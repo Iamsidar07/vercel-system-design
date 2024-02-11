d1372a3f0ccc5ee5e14606e10c081e5ff7b0f5c3
Manoj Kumar <ms8460149@gmail.com>
2024-02-11 15:41:39 +0530

    feat: included with another methods

--- vercel-upload-service/src/file.ts
+++ vercel-upload-service/src/file.ts
@@ -2,18 +2,27 @@ import fs from "fs"
 import path from "path"
 
 export function getAllFilePaths(folderPath: string) {
-    let response: string[] = []
-    const allFilesAndFolders = fs.readdirSync(folderPath)
-    allFilesAndFolders.forEach(file => {
-        const fullFilePath = path.join(folderPath, file)
+    // let response: string[] = []
+    // const allFilesAndFolders = fs.readdirSync(folderPath)
+    // allFilesAndFolders.forEach(file => {
+    //     const fullFilePath = path.join(folderPath, file)
 
-        const isDirectory = fs.statSync(fullFilePath).isDirectory()
-        if (isDirectory) {
-            response = response.concat(getAllFilePaths(fullFilePath))
-        } else {
-            response.push(fullFilePath)
+    //     const isDirectory = fs.statSync(fullFilePath).isDirectory()
+    //     if (isDirectory) {
+    //         response = response.concat(getAllFilePaths(fullFilePath))
+    //     } else {
+    //         response.push(fullFilePath)
+    //     }
+    // });
+    // return response
+
+    const allFilesAndFolders = fs.readdirSync(folderPath, { recursive: true })
+    const response = []
+    for (const fileOrFolder of allFilesAndFolders) {
+        const fileOrFolderPath = path.join(folderPath, fileOrFolder as string)
+        if (!fs.statSync(fileOrFolderPath).isDirectory()) {
+            response.push(fileOrFolderPath)
         }
-    });
+    }
     return response
 }
-
